4d6438f1a9b46ce1abcded3ac1708a40e8290802
Collin Jones <jonesc11@rpi.edu>
2017-09-28 13:36:56 -0400

    cdj - Completed db-connect.php with empty username, password, and host fields.

--- lib/db-connect.php
+++ lib/db-connect.php
@@ -1,5 +1,21 @@
 <?php
 
+    $username = '';
+    $password = '';
+    $host     = '';
+    $dbname   = 'ratemycourses';
     
-
-?>
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    
+    $errorMessage  = "Error connecting to database. Please contact <a href=\"mailto:jonesc11@rpi.edu\">jonesc11@rpi.edu</a>\n";
+    
+    try {
+        $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
+    } catch (PDOException $e) {
+        die ($errorMessage);
+    }
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+    session_start();
