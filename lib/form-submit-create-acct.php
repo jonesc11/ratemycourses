4b24a7cc2d44007b9a98c6930b364a7283a59862
Collin Jones <jonesc11@rpi.edu>
2017-10-12 08:42:08 -0400

    cdj - Create form-submit-create-acct.php handler. Built to be used with AJAX.

--- lib/form-submit-create-acct.php
+++ lib/form-submit-create-acct.php
@@ -12,11 +12,11 @@
      *                  Errors will use Bootstrap alerts.
      */
     
-    $fname     = $_POST['firstname'];
-    $lname     = $_POST['lastname'];
-    $uname     = $_POST['username'];
-    $email     = $_POST['email'];
-    $confEmail = $_POST['conf_email'];
+    $fname     = trim($_POST['firstname']);
+    $lname     = trim($_POST['lastname']);
+    $uname     = trim($_POST['username']);
+    $email     = trim($_POST['email']);
+    $confEmail = trim($_POST['conf_email']);
     $password  = $_POST['password'];
     $confPass  = $_POST['conf_password'];
     
@@ -31,26 +31,85 @@
     $continue    = TRUE;
     
     //- Check if username is in use or invalid.
-    
+    if (!ctype_alnum($uname) && $continue) {
+        $arr = array('error' => 'Username must be alphanumeric.',
+                     'email' => $email,
+                     'focus' => 'username',
+                     'firstname' => $fname,
+                     'lastname' => $lname);
+        echo genCreateForm($arr);
+        $continue = FALSE;
+    }
     
     //- Check if email is in use or invalid.
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL) && $continue) {
+        $arr = array('error' => 'Please enter a valid email address.',
+                     'username' => $uname,
+                     'focus' => 'email',
+                     'firstname' => $fname,
+                     'lastname' => $lname);
+        echo genCreateForm($arr);
+        $continue = FALSE;
+    }
     
+    $statement = $db->prepare('SELECT email,active FROM `users`;');
+    $result = $statement->execute();
+    
+    while ($row = $statement->fetch() && $continue) {
+        if (strtolower($row['email']) == strtolower($email) && $row['active'] == 1) {
+            $arr = array('error' => 'Email in use, please use a different username.',
+                         'username' => $uname,
+                         'focus' => 'email',
+                         'firstname' => $fname,
+                         'lastname' => $lname);
+            echo genCreateForm($arr);
+            $continue = FALSE;
+        }
+    }
     
     //- Check if email matches conf_email.
-    
+    if (strtolower($email) != strtolower($confEmail) && $continue) {
+        $arr = array('error' => 'Emails do not match, please try again.',
+                     'username' => $uname,
+                     'focus' => 'email',
+                     'firstname' => $fname,
+                     'lastname' => $lname);
+        echo genCreateForm($arr);
+        $continue = FALSE;
+    }
     
     //- Check to see if password is powerful enough.
-    
+    if (!(preg_match('~[A-Z]~', $_POST['password']) && 
+          preg_match('~[a-z]~', $_POST['password']) &&
+          preg_match('~[0-9]~', $_POST['password'])
+          ) && $continue) {
+        $arr = array('error' => 'Password must contain at least 1 uppercase character, lowercase character, and number.',
+                     'username' => $uname,
+                     'focus' => 'password',
+                     'firstname' => $fname,
+                     'lastname' => $lname,
+                     'email' => $email);
+        echo genCreateForm($arr);
+        $continue = FALSE;
+    }
     
     //- Check to see if password matches conf_password.
-    
+    if ($password != $confPass && $continue) {
+        $arr = array('error' => 'Passwords do not match.',
+                     'username' => $uname,
+                     'focus' => 'password',
+                     'firstname' => $fname,
+                     'lastname' => $lname);
+        echo genCreateForm($arr);
+        $continue = FALSE;
+    }
     
     //- Submit the form (using function in auth-helpers.php) and redirect.
     if ($continue) {
         if (createNewUser($username, $firstname, $lastname, strtolower($email), $password, $db, $dbname))
-            header ('Location: ' . $successPage);
+            echo '<script>window.location = "' . $successPage . '"</script>';
         else
-            header ('Location: ' . $failurePage);
+            echo '<script>window.location = "' . $successPage . '"</script>';
     }
     
 ?>
