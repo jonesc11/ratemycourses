c2dc345ec0cd3fb1b0cb8a65f7b8461f8e0958a6
Collin Jones <jonesc11@rpi.edu>
2017-10-15 09:35:33 -0400

    cdj - Minor fixes to the navbar script.

--- lib/navbar.php
+++ lib/navbar.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<? php require_once('auth_helpers.php'); ?>
+<?php require_once(__DIR__ . DIRECTORY_SEPARATOR . 'auth-helpers.php'); ?>
 <html>
   <head>
     <meta charset="utf-8">
