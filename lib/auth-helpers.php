8b2f99dfe138ad1434378221baa0c762cd7d1589
Khalil Fleming <khalilfleming@Khalils-MacBook-Pro.local>
2017-10-17 19:25:52 -0400

    kf updated auth-helpers for login

--- lib/auth-helpers.php
+++ lib/auth-helpers.php
@@ -53,7 +53,97 @@
     }
     
     function getErrorMessage($errorKey) {
-        
+    	    
     }
+	
+	function isLoggedIn(){ //determines whether user is logged in or not
+		return isset($_SESSION["user"]) && isValid($_SESSION["user"]["email"], $_SESSION["user"]["password"]);
+	}
+
+
+	//Determines whether or not uname and password combination is correct
+	//pulls database values and checks against hashed password
+
+	function isValid($id, $hash_password){
+		$isEmail = false;
+		
+		if(strpos($id,'@') !== false){
+			$isEmail = true;
+		}
+		
+		$query = "SELECT * FROM `users`;";
+        $statement = $db->prepare($query);
+        $result = $statement->execute();
+        
+        while ($row = $statement->fetch()) {
+            if ($isEmail == true){
+				if(strtolower($id) == strtolower($row["email"])){
+					return $hash_password == $row["password"];
+				}
+			}
+			else{
+				if(strtolower($id) == strtolower($row["username"])){
+					return $hash_password == $row["password"];
+				}
+			}
+        }
+		
+		return false;
+	}
+
+	//Logs in the user if uname or password is correct
+
+	function login($id,$password){
+		$isEmail = false;
+		
+		if(strpos($id,'@') !== false){
+			$isEmail = true;
+		}
+		
+		$query = "SELECT * FROM `users`;";
+        $statement = $db->prepare($query);
+        $result = $statement->execute();
+        
+        while ($row = $statement->fetch()) {
+            if ($isEmail == true){
+				if(strtolower($id) == strtolower($row["email"])){
+					$salt = $row["salt"];
+					break;
+				}
+			}
+			else{
+				if(strtolower($id) == strtolower($row["username"])){
+					$salt = $row["salt"];
+					break;
+				}
+			}
+        }
+		if (!isset($salt)){
+			return false;
+		}
+		
+		
+		
+		$password = genHashedPassword($password,$salt);
+			
+		if(isValid($username,$password) == true){
+			$_SESSION["user"]["id"] = $row["id"];
+			$_SESSION["user"]["firstname"] = $row["firstname"];
+			$_SESSION["user"]["lastname"] = $row["lastname"];
+			$_SESSION["user"]["password"] = $row["password"];
+			$_SESSION["user"]["username"] = $row["username"];
+			$_SESSION["user"]["email"] = $row["email"];
+			$_SESSION["user"]["permissions"] = $row["permissions"];
+			return true;
+		}
+		return false;
+	}
+	
+	//logs user out
+	function logout(){
+		if(isset($_SESSION["user"])){
+			unset($_SESSION["user"]);
+		}
+	}
     
 ?>
