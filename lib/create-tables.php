297a6d6342d30fd04a265e60fe59a6fcf15372b5
Collin Jones <jonesc11@rpi.edu>
2017-11-16 12:15:40 -0500

    cdj - Fixed error

--- lib/create-tables.php
+++ lib/create-tables.php
@@ -99,7 +99,7 @@
     $query .= 'major varchar(4) NOT NULL,';
     $query .= 'coursenum int NOT NULL,';
     $query .= 'userid varchar(8) DEFAULT \'FFFFFFFF\',';
-    $query .= 'suggestion varchar(2048) NOT NULL';
+    $query .= 'suggestion varchar(2048) NOT NULL,';
     $query .= 'status int DEFAULT 0,';
     $query .= 'PRIMARY KEY(id)';
     $query .= ');';
