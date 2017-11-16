fd5de4ba61edfbdad349d8f10063d35f4e0b539b
Collin Jones <jonesc11@rpi.edu>
2017-11-16 11:55:22 -0500

    cdj - Changes to table structure.

--- lib/create-tables.php
+++ lib/create-tables.php
@@ -30,6 +30,7 @@
     $query .= 'coursename varchar(54) NOT NULL,';
     $query .= 'major varchar(4) NOT NULL,';
     $query .= 'coursenum int NOT NULL,';
+    $query .= 'schoolid varchar(8) NOT NULL,';
     $query .= 'PRIMARY KEY(id)';
     $query .= ');';
     
@@ -113,7 +114,22 @@
     $query .= 'major varchar(4) NOT NULL,';
     $query .= "school enum('HASS','SCI','ENG','BUS','ARCH','OTH') NOT NULL,";
     $query .= 'name varchar(50) NOT NULL,';
+    $query .= 'schoolid varchar(8) NOT NULL,';
     $query .= 'PRIMARY KEY(major)';
+    $query .= ');';
+    
+    try {
+        $statement = $db->prepare($query);
+        if ($statement->execute() === FALSE)
+            die ($errorMessage . "suggestions");
+    } catch (Exception $e) {
+        die ($errorMessage . "suggestions");
+    }
+    
+    $query  = 'CREATE TABLE schools (';
+    $query .= 'id varchar(8) NOT NULL,';
+    $query .= 'name varchar(50) NOT NULL,';
+    $query .= 'PRIMARY KEY(id)';
     $query .= ');';
     
     try {
