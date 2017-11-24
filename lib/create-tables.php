175fe6955eb2f1ffbc510567dee5db16740e6684
Yarden <yardenn1@gmail.com>
2017-11-23 20:21:20 -0500

    Updated majors table structure

--- lib/create-tables.php
+++ lib/create-tables.php
@@ -4,7 +4,7 @@
     
     $errorMessage = "Error creating table: ";
     
-    $query  = 'CREATE TABLE users (';
+    $query  = 'CREATE TABLE IF NOT EXISTS users (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'username varchar(32) NOT NULL,';
     $query .= 'password varchar(128) NOT NULL,';
@@ -25,7 +25,7 @@
         die ($errorMessage . "users");
     }
     
-    $query  = 'CREATE TABLE courses (';
+    $query  = 'CREATE TABLE IF NOT EXISTS courses (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'coursename varchar(54) NOT NULL,';
     $query .= 'major varchar(4) NOT NULL,';
@@ -42,7 +42,7 @@
         die ($errorMessage . "courses");
     }
     
-    $query  = 'CREATE TABLE votes (';
+    $query  = 'CREATE TABLE IF NOT EXISTS votes (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'vote int NOT NULL,';
     $query .= 'commentid varchar(8) NOT NULL,';
@@ -57,7 +57,7 @@
         die ($errorMessage . "votes");
     }
     
-    $query  = 'CREATE TABLE comments (';
+    $query  = 'CREATE TABLE IF NOT EXISTS comments (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'courseid varchar(8) NOT NULL,';
     $query .= 'comment varchar(2048) NOT NULL,';
@@ -76,7 +76,7 @@
         die ($errorMessage . "comments");
     }
     
-    $query  = 'CREATE TABLE ratings (';
+    $query  = 'CREATE TABLE IF NOT EXISTS ratings (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'category1 int DEFAULT NULL,';
     $query .= 'category2 int DEFAULT NULL,';
@@ -94,7 +94,7 @@
         die ($errorMessage . "ratings");
     }
     
-    $query  = 'CREATE TABLE suggestions (';
+    $query  = 'CREATE TABLE IF NOT EXISTS suggestions (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'major varchar(4) NOT NULL,';
     $query .= 'coursenum int NOT NULL,';
@@ -112,23 +112,24 @@
         die ($errorMessage . "suggestions");
     }
     
-    $query  = 'CREATE TABLE majors (';
-    $query .= 'major varchar(4) NOT NULL,';
-    $query .= "school enum('HASS','SCI','ENG','BUS','ARCH','OTH') NOT NULL,";
+    $query  = 'CREATE TABLE IF NOT EXISTS majors (';
+    $query .= 'id varchar(8) NOT NULL,';
+    $query .= 'major varchar(10) NOT NULL,';
+    $query .= 'school varchar(20) NOT NULL,';
     $query .= 'name varchar(50) NOT NULL,';
     $query .= 'schoolid varchar(8) NOT NULL,';
-    $query .= 'PRIMARY KEY(major)';
+    $query .= 'PRIMARY KEY(id)';
     $query .= ');';
     
     try {
         $statement = $db->prepare($query);
         if ($statement->execute() === FALSE)
-            die ($errorMessage . "suggestions");
+            die ($errorMessage . "majors");
     } catch (Exception $e) {
-        die ($errorMessage . "suggestions");
+        die ($errorMessage . "majors");
     }
     
-    $query  = 'CREATE TABLE schools (';
+    $query  = 'CREATE TABLE IF NOT EXISTS schools (';
     $query .= 'id varchar(8) NOT NULL,';
     $query .= 'name varchar(50) NOT NULL,';
     $query .= 'PRIMARY KEY(id)';
@@ -137,9 +138,9 @@
     try {
         $statement = $db->prepare($query);
         if ($statement->execute() === FALSE)
-            die ($errorMessage . "suggestions");
+            die ($errorMessage . "schools");
     } catch (Exception $e) {
-        die ($errorMessage . "suggestions");
+        die ($errorMessage . "schools");
     }
     
 ?>
