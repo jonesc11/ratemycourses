86bd225268fae384bf5a33b80882b922cc2b56c6
Collin Jones <jonesc11@rpi.edu>
2017-11-24 20:28:17 -0500

    cdj - a ton of shit again

--- createcomment/index.php
+++ createcomment/index.php
@@ -3,6 +3,10 @@
     require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'db-connect.php');
     require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'comment-helpers.php');
     
+    if (!isset($_SESSION['user']) /*|| $_SESSION['user']['active'] != 1*/)
+      //  header ("Location: /login");
+        $_SESSION['user']['username'] = "collin"; // REMEMBER TO REMOVE THIS
+    
     if (isset($_GET['c'])) {
         $coursename = getCourseName($_GET['c']);
     } else {
@@ -31,5 +35,43 @@
           die();
       }
     ?>
+    <form action="/lib/form-submit-create-comment.php" method="POST">
+      <input type="hidden" name="courseid" value="<?php echo $_GET['c']; ?>" />
+      <input type="hidden" name="userid" value="<?php echo $_SESSION['user']['username'] ?>" />
+      <label>Enter your comment here</label><textarea name="comment" rows="4" cols="50"></textarea>
+      <label>Difficulty (1 - easy A, 5 - good luck not dropping)</label>
+      <select name="rating1">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <label>Workload (1 - little to no homework, 5 - no sleep)</label>
+      <select name="rating2">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <label>Attendance (1 - unnecessary, 5 - critical)</label>
+      <select name="rating3">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <label>Interesting (1 - fall asleep, 5 - didn't miss a word)</label>
+      <select name="rating4">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <input type="submit" value="Submit" name="submit" />
+    </form>
   </body>
 </html>
--- lib/comment-helpers.php
+++ lib/comment-helpers.php
@@ -64,7 +64,7 @@
         $query = "INSERT INTO `comments` (`id`, `courseid`, `comment`, `ratingid`, `userid`) VALUES (:id, :courseid, :comment, :ratingid, :userid);";
         $param = array (':id' => $newId, ':courseid' => $array['courseid'], ':comment' => $array['comment'], ':ratingid' => $array['ratingid'], ':userid' => $array['userid']);
         
-        $statement = $db->prepare($statement);
+        $statement = $db->prepare($query);
         $result = $statement->execute($param);
         
         if ($result !== FALSE)
@@ -87,7 +87,7 @@
         $query = "INSERT INTO `ratings` (`id`, `category1`, `category2`, `category3`, `category4`, `category5`) VALUES (:id, :c1, :c2, :c3, :c4, :c5);";
         $param = array (':id' => $newId, ':c1' => $array['rating1'], ':c2' => $array['rating2'], ':c3' => $array['rating3'], ':c4' => $array['rating4'], ':c5' => $array['rating5']);
         
-        $statement = $db->prepare($statement);
+        $statement = $db->prepare($query);
         $result = $statement->execute($param);
         
         if ($result !== FALSE)
@@ -149,7 +149,39 @@
             while ($row = $statement->fetch())
                 $comments[] = $row;
             
+            $ratings = array(0,0,0,0,0);
+            
+            //- Convert ratings from ratings table to comments.
+            foreach ($comments as $key => $comment) {
+                $statement = $db->prepare("SELECT * FROM `ratings` WHERE `id` = :id;");
+                $statement->execute(array(':id' => $comment['ratingid']));
+                
+                $row = $statement->fetch();
+                
+                //- Set comments ratings
+                $comments[$key]['rating1'] = $row['category1'];
+                $comments[$key]['rating2'] = $row['category2'];
+                $comments[$key]['rating3'] = $row['category3'];
+                $comments[$key]['rating4'] = $row['category4'];
+                $comments[$key]['rating5'] = $row['category5'];
+                
+                //- For averages
+                $ratings[0] += $row['category1'];
+                $ratings[1] += $row['category2'];
+                $ratings[2] += $row['category3'];
+                $ratings[3] += $row['category4'];
+                $ratings[4] += $row['category5'];
+            }
+            
+            //- Calculate averages
+            $ratings[0] /= count($comments);
+            $ratings[1] /= count($comments);
+            $ratings[2] /= count($comments);
+            $ratings[3] /= count($comments);
+            $ratings[4] /= count($comments);
+            
             if (count($comments) == 0) {
+                //- Display no posts yet
                 $ret['title'] = $course['coursename'] . ' - RateMyCourses';
                 
                 $ret['content']  = '<div id="content">';
@@ -157,7 +189,40 @@
                 $ret['content'] .= '<a href="/createcomment?c=' . $courseid . '" title="Review this course">Review this course</a>';
                 $ret['content'] .= '</div>';
             } else {
+                $ret['title'] = $course['coursename'] . ' - RateMyCourses';
                 
+                //- Overview - averages, etc
+                $ret['content']  = '<div id="content">';
+                $ret['content'] .= '<h1>' . $course['coursename'] . '</h1>';
+                $ret['content'] .= '<h2>Overview:</h2>';
+                $ret['content'] .= '<h3>Difficulty:</h3><p>' . number_format($ratings[0],1) . ' / 5</p><img src="/resources/images/rating' . round($ratings[0]) . '.jpg" alt="Rating of ' . number_format($ratings[0],1) . '" />';
+                $ret['content'] .= '<h3>Workload:</h3><p>' . number_format($ratings[1],1) . ' / 5</p><img src="/resources/images/rating' . round($ratings[1]) . '.jpg" alt="Rating of ' . number_format($ratings[1],1) . '" />';
+                $ret['content'] .= '<h3>Attendance:</h3><p>' . number_format($ratings[2],1) . ' / 5</p><img src="/resources/images/rating' . round($ratings[2]) . '.jpg" alt="Rating of ' . number_format($ratings[2],1) . '" />';
+                $ret['content'] .= '<h3>Interesting:</h3><p>' . number_format($ratings[3],1) . ' / 5</p><img src="/resources/images/rating' . round($ratings[3]) . '.jpg" alt="Rating of ' . number_format($ratings[3],1) . '" />';
+                
+                //- Counter for paginating.
+                $counter = 0;
+                
+                //- List all comments.
+                foreach ($comments as $comment) {
+                    if ($counter >= $offset && $counter < $offset + 25) {
+                        $ret['content'] .= '<div class="comment">';
+                        $ret['content'] .= '<div class="comment-ratings">';
+                        $ret['content'] .= '<h3>Difficulty:</h3><p>' . $comment['rating1'] . ' / 5</p><img src="/resources/images/rating' . $comment['rating1'] . '.jpg" alt="Rating of ' . $comment['rating1'] . '" />';
+                        $ret['content'] .= '<h3>Workload:</h3><p>' . $comment['rating2'] . ' / 5</p><img src="/resources/images/rating' . $comment['rating2'] . '.jpg" alt="Rating of ' . $comment['rating2'] . '" />';
+                        $ret['content'] .= '<h3>Attendance:</h3><p>' . $comment['rating3'] . ' / 5</p><img src="/resources/images/rating' . $comment['rating3'] . '.jpg" alt="Rating of ' . $comment['rating3'] . '" />';
+                        $ret['content'] .= '<h3>Interesting:</h3><p>' . $comment['rating4'] . ' / 5</p><img src="/resources/images/rating' . $comment['rating4'] . '.jpg" alt="Rating of ' . $comment['rating4'] . '" />';
+                        $ret['content'] .= '</div>';
+                        $ret['content'] .= '<div class="comment-content">';
+                        $ret['content'] .= $comment['comment'];
+                        $ret['content'] .= '</div>';
+                        $ret['content'] .= '</div>';
+                    }
+                    
+                    $counter++;
+                }
+                
+                $ret['content'] .= '</div>';
             }
         } else {
             $ret = courseNotFound();
