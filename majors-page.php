4197b8244fce6c87c1e806e6e79b299adebd3644
Yarden <yardenn1@gmail.com>
2017-11-26 00:59:43 -0500

    Added title to majors page

--- majors-page.php
+++ majors-page.php
@@ -2,6 +2,7 @@
 <?php require_once(__DIR__ . DIRECTORY_SEPARATOR . 'lib/browse-helpers.php'); ?>
 <html>
   <head>
+    <title>Majors - RateMyCourses</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
