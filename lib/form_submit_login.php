5a97a463b7e923630c5256da33a54965d37cee8c
Khalil Fleming <khalilfleming34@gmail.com>
2017-11-27 19:35:24 -0500

    login form handler

---
+++ lib/form_submit_login.php
@@ -0,0 +1,19 @@
+<?php
+    require_once (__DIR__ . DIRECTORY_SEPARATOR . 'db-connect.php');
+    require_once (__DIR__ . DIRECTORY_SEPARATOR . 'auth-helpers.php'); 
+
+    if(isset($_POST["username"]))
+        $username = $_POST["username"];
+    if(isset($_POST["password"]))
+        $password = $_POST["password"];
+    
+    $ret = login($username, $password);
+    if ($ret == true){
+        echo '<script> window.location = "/"; </script>';        
+    }
+    else{
+        echo genLoginForm(array("error" => "Username and password do not match.", "username" => $username));
+    }
+        
+
+?>
